78a127d929a03431b2560170c449b7da3b6844a0
Avery Pratt <apratt91@gmail.com>
2016-10-05 16:08:41 -0700

    added pictures

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ var imgContainer = document.getElementById('image-container');
 var productImg = [];
 var newImg = [];
 var previousImages = [];
-// product constructor
+var left = document.getElementById('left');
+var middle = document.getElementById('middle');
+var right = document.getElementById('right');
 
-function Product(imgName, path, views, clicks) {
-  this.imgName = imgName;
-  this.path = path;
-  this.views = views;
-  this.click = clicks;
-  productImg.push(this);
-};
+var leftImg;
+var centerImg;
+var rightImg;
 
 new Product('bag', 'img/bag.jpg', 0, 0);
 new Product('banana', 'img/banana.jpg', 0, 0);
@@ -34,19 +32,21 @@ new Product('usb', 'img/usb.gif', 0, 0);
 new Product('water-can', 'img/water-can.jpg', 0, 0);
 new Product('wine-glass', 'img/wine-glass.jpg', 0, 0);
 
+// product constructor
+function Product(imgName, path, views, clicks) {
+  this.imgName = imgName;
+  this.path = path;
+  this.views = views;
+  this.click = clicks;
+  productImg.push(this);
+};
 
-
-var left = document.getElementById('left');
-var middle = document.getElementById('middle');
-var right = document.getElementById('right');
-
-var leftImg;
-var centerImg;
-var rightImg;
-
-render();
-
-
+function turnArrayIntoImages(product, img){
+  var att = document.createAttribute('src');
+  att.value = product.path;
+  img.setAttributeNode(att);
+  // left.setAttribute();
+}
 
 function render() {
   var imgRandom = function() {
@@ -57,11 +57,13 @@ function render() {
     newImg.push(leftImg, centerImg, rightImg);
   };
   imgRandom();
-  productImg[leftImg];
-
-
   //checking for duplicate images
   while ((leftImg === centerImg) || (centerImg === rightImg) || (leftImg === rightImg) || newImg[0] === previousImages[0] || newImg[0] === previousImages[1] || newImg[0] === previousImages[2] || newImg[1] === previousImages[0] || newImg[1] === previousImages[1] || newImg[1] === previousImages[2] || newImg[2] === previousImages[0] || newImg[2] === previousImages[1] || newImg[2] === previousImages[2]) {
     imgRandom();
   }
+  turnArrayIntoImages(productImg[leftImg], left);
+  turnArrayIntoImages(productImg[centerImg], middle);
+  turnArrayIntoImages(productImg[rightImg], right);
 }
+
+render();
