aca962642e68b491c7752a3c4f6b70d972fa5a0d
sueanyv <valenciasue@hotmail.com>
2016-10-04 18:50:54 -0700

    making constuctor

---
+++ apps.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+var allproducts = [];
+
+
+
+// product constructor
+
+function Product(name, path, views, clicks) {
+  this.name = name;
+  this.path = path;
+  this.views = views;
+  this.click = clicks;
+}
