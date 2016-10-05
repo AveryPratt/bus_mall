404b04a5d1a8003925906216ddb72e215a6f0299
sueanyv <valenciasue@hotmail.com>
2016-10-04 19:03:02 -0700

    added images

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var allproducts = [];
+var productimg = [];
 
 
 
@@ -12,4 +12,25 @@ function Product(name, path, views, clicks) {
   this.path = path;
   this.views = views;
   this.click = clicks;
+  productimg.push(this);
 }
+
+new Product('bag', 'img/bag.jpg');
+new Product('banana', 'img/banana.jpg');
+new Product('bathroom', 'img/bathroom.jpg');
+new Product('boots', 'img/boots.jpg');
+new Product('breakfast', 'img/breakfast.jpg');
+new Product('bubblegum', 'img/bubblegum.jpg');
+new Product('chair', 'img/chair.jpg');
+new Product('cthulhu', 'img/cthulhu.jpg');
+new Product('dogduck', 'img/dog-duck.jpg');
+new Product('pen', 'img/pen.jpg');
+new Product('petsweep', 'img/pet-sweep.jpg');
+new Product('scissors', 'img/scissors.jpg');
+new Product('shark', 'img/shark.jpg');
+new Product('sweep', 'img/sweep.jpg');
+new Product('tautaun', 'img/tauntaun.jpg');
+new Product('unicorn', 'img/unicorn.jpg');
+new Product('usb', 'img/usb.jpg');
+new Product('watercan', 'img/water-can.jpg');
+new Product('wine-glass', 'img/wine-glass.jpg');
